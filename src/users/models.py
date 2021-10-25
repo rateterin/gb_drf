c93c73b393961bab7119d465dfab06401c485cc9
rateterin <rateterin@yandex.ru>
2021-10-26 01:26:10 +0300

    created User model

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=64, verbose_name='имя')
+    last_name = models.CharField(max_length=64, verbose_name='фамилия')
+    email = models.CharField(
+        primary_key=True,
+        verbose_name='электронная почта'
+    )
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
