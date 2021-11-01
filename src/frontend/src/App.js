32f56e9b3976cb8cf790fb47a399acc5062f39c1
rateterin <rateterin@yandex.ru>
2021-11-01 17:22:01 +0300

    Added componentDidMount with static data fo two users; Correct render() for render List of users.

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,25 +1,46 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import UserList from "./User";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'users': []
+        }
+    }
+
+    componentDidMount() {
+       const users = [
+           {
+               'first_name': 'Фёдор',
+               'last_name': 'Достоевский',
+               'email': 'fedor_dostoyevskiy@example.com'
+           },
+           {
+               'first_name': 'Александр',
+               'last_name': 'Грин',
+               'email': 'alex_green@example.com'
+           },
+       ]
+       this.setState(
+           {
+               'users': users
+           }
+       )
+
+    }
+
+    render() {
+        return (
+            <div>
+                <UserList users={this.state.users} />
+            </div>
+        )
+    }
 }
+
 
 export default App;
