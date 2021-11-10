1cd1433baf7d41fe6358015c1241088ef4b090bb
rateterin <rateterin@yandex.ru>
2021-11-10 21:55:29 +0300

    Finalize lesson_3 practise

--- src/todo_app/admin.py
+++ src/todo_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo_app.models import Project, Todo
+
+admin.site.register(Project)
+admin.site.register(Todo)
--- src/todo_app/models.py
+++ src/todo_app/models.py
@@ -3,13 +3,31 @@ from django.db import models
 
 class Project(models.Model):
     name = models.CharField(max_length=32, blank=False, null=False)
-    link_to_repo = models.CharField(max_length=64)
+    link_to_repo = models.CharField(max_length=64, blank=True, null=True)
     users = models.ManyToManyField('users.User', related_name='project_users')
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+
+    def __str__(self):
+        return f'Проект "{self.name}"'
 
 
 class Todo(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project,
+                                on_delete=models.CASCADE,
+                                related_name='todo_project')
     text = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    user_created = models.OneToOneField('users.User', on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_date = models.DateTimeField(auto_now=True, editable=False)
+    user_created = models.OneToOneField('users.User',
+                                        on_delete=models.CASCADE,
+                                        editable=False)
+
+    class Meta:
+        verbose_name = 'ToDo заметка'
+        verbose_name_plural = 'ToDo заметки'
+
+    def __str__(self):
+        return f'ToDo заметка для проекта "{Todo.project.name}"'
