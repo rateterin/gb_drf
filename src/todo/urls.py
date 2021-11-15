c10a2e33f7bdcdeed93b3e73440d2d0e5ec4d745
rateterin <rateterin@yandex.ru>
2021-11-15 19:38:57 +0300

    lesson 4 progress

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -18,11 +18,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from todo_app.views import ProjectModelViewSet, TodoModelViewSet
-from users.views import UserModelViewSet
+from users.views import UserCustomViewSet
 
 
 router = DefaultRouter()
-router.register('users', UserModelViewSet)
+router.register('users', UserCustomViewSet)
+router.register('users/<int:pk>', UserCustomViewSet)
 router.register('projects', ProjectModelViewSet)
 router.register('todo', TodoModelViewSet)
 
--- src/users/views.py
+++ src/users/views.py
@@ -1,8 +1,10 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet, mixins
 from .models import User
 from .serializers import UserSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserCustomViewSet(mixins.ListModelMixin,
+                        mixins.UpdateModelMixin,
+                        GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
